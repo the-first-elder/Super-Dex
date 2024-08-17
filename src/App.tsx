7ca260f73addbfc5194b76a6c574d0f3024d566c
the-first-elder <emmanuelmayowa95@gmail.com>
2024-08-17 22:17:08 +0100

    b

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // import React from "react";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
-import LoginForm from "./components/LoginForm";
+// import Footer from "./components/Footer";
+// import LoginForm from "./components/LoginForm";
 
 function App() {
   return (
